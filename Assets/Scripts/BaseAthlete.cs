0e9553f4d4ba461194bb73cc7424ce780921ffd2
Isaac Scheff <isaac1996@comcast.net>
2024-04-03 17:22:47 -0400

    Initial project files

---
+++ Assets/Scripts/BaseAthlete.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAthlete : MonoBehaviour {
+    [SerializeField] private int _baseHealth;
+    [SerializeField] private int _baseSpeed;
+    [SerializeField] private int _baseBatAccuracy;
+    [SerializeField] private int _baseBatPower;
+    [SerializeField] private int _basePitching;
+    [SerializeField] private int _baseCatching;
+    [SerializeField] private int _baseFielding;
+    [SerializeField] private int _baseStamina;
+}
